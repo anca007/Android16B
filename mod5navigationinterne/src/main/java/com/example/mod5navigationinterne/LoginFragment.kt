5836f0c01e3f4c5b3d11a02a6bf06163094b6a63
stropee <stropee@campus-eni.fr>
2023-12-06 16:54:09 +0100

    mod5demoNavigationInterne

---
+++ mod5navigationinterne/src/main/java/com/example/mod5navigationinterne/LoginFragment.kt
@@ -0,0 +1,38 @@
+package com.example.mod5navigationinterne
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.mod5navigationinterne.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    //méthode appelée une fois que la vue ets créée !
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            //on navigue vers l'id de la flèche
+            Navigation.findNavController(view).navigate(R.id.loginFragmentToHomeFragment)
+
+        }
+
+    }
+
+
+}
